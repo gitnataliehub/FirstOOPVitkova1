9859b84c6aa61b8eb6c8d19419f66fbc967338f8
Наталя <ntlvitkova@gmail.com>
2024-04-02 12:20:31 +0300

    змінила тип данних int на short

--- FirstOOPVitkova/src/Goods.java
+++ FirstOOPVitkova/src/Goods.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Goods {
+public class Goods {
+    private String name;// назва товару
+    private short count;// кількість товару
+    private double price;// ціна товару
 }
+
+public static double getSum(int count, double price) { // функція підрахунку суми
+        return count*price;   // повертає результат обчислення
+}
+public static void main(String[] args) {
+    System.out.println(getSum(5,20.5)); // виводить результат функції з введенними значеннями
+    }
+
+
