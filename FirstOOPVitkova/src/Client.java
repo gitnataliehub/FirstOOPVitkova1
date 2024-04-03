f2f3659e167d60156161501ccf1f82da6eb8a02a
Наталя <ntlvitkova@gmail.com>
2024-04-03 21:22:27 +0300

    Додала до класів Client та Goods конструктор, і об'єкти

--- FirstOOPVitkova/src/Client.java
+++ FirstOOPVitkova/src/Client.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Client {
+public static class Client {
+    private String name;
+    private int id;
+    private long phone;
+    private long numberCard;
+
+    public Client(String name, int id, long phone, long numberCard) { //конструктор класу Клієнт
+        this.name = name;
+        this.id = id;
+        this.phone = phone;
+        this.numberCard = numberCard;
+    }
+}
+
+public static long getContact(long phone) {
+    return phone;
+}
+
+public static void main(String[] args) {
+    Client client = new Client("Володимир Линчук", 123, 380_15_144_51_15L, 4157_1515_5555_0125L);
+    Client client1 = new Client("Іванна Мудрик", 124, 380_16_044_51_15L, 4158_1515_5555_0125L);
+    System.out.println(getContact(1));
+    System.out.println(getContact(client.phone));
 }
--- FirstOOPVitkova/src/Goods.java
+++ FirstOOPVitkova/src/Goods.java
@@ -1,14 +1,23 @@
-public class Goods {
+public static class Goods {
     private String name;// назва товару
     private short count;// кількість товару
     private double price;// ціна товару
+
+    public Goods(String name, short count, double price) { //конструктор класу Товари
+        this.name = name;
+        this.count = count;
+        this.price = price;
+    }
 }
 
-public static double getSum(int count, double price) { // функція підрахунку суми
-        return count*price;   // повертає результат обчислення
+public static double getSum(short count, double price) { // функція підрахунку суми
+    return count * price;   // повертає результат обчислення
 }
+
 public static void main(String[] args) {
-    System.out.println(getSum(5,20.5)); // виводить результат функції з введенними значеннями
-    }
+    Goods sofa = new Goods("Диван", (short) 5, 3250.5); //створення об'єкта Диван
+    Goods table = new Goods("Стіл", (short) 10, 250); // створення об'єкта Стіл
+    System.out.println(getSum((short) 5, 3250.5)); // виводить результат функції з введенними значеннями
+}
 
 
